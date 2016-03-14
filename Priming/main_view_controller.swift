a7da2fb19dc2ea21d24056ee49f5e1561e91008a
Kim <kwb425@icloud.com>
2016-03-15 02:54:51 +0900

    Priming
    so far,
    1. Auto-layouted.
    2. Design completed.
    3. Layout-code connections established.
    4. e-mail detail added.
    5. README.md added
    6. Data links between main & sub view controllers are setted.

--- Priming/main_view_controller.swift
+++ Priming/main_view_controller.swift
@@ -19,11 +19,14 @@ class main_view_controller: UIViewController {
     
     
     ///////////////////////// Outlets /////////////////////////
+    // Outlets whos informations will be fed into a dictionary
     @IBOutlet weak var student_number: UITextField!
     @IBOutlet weak var student_name: UITextField!
     @IBOutlet weak var student_department: UITextField!
     @IBOutlet weak var student_lecture: UITextField!
-    @IBOutlet weak var left_button: UISwitch!
+    @IBOutlet weak var left_handed: UISwitch!
+    // The dictionary to be passed to sub_view_controller
+    var passing_data = [String: String]()
     
     
     
@@ -33,64 +36,20 @@ class main_view_controller: UIViewController {
     
     
     
-    ////////////////////////
-    // Start button callback
-    ////////////////////////
-    @IBAction func start_button(sender: AnyObject) {
-        
-        //// 1. Send a request to server (Mailgun).
-        let request: NSMutableURLRequest = NSMutableURLRequest(URL: NSURL(string: "https://api.mailgun.net/v3/sandbox15cbe9dbdda946eebdea18594ab7d313.mailgun.org/messages")!)
-        
-        // Set a type of communication between a client (user) and the server.
-        request.HTTPMethod = "POST" // 'POST' request is used to send data to the server.
-        
-        //// 2. You have to authenticate (ID, Password) to the Mailgun to send mails through it.
-        let username = "api"
-        let password = "key-13bf8dc7873358d4352e591919b48616"
-        // Making a login information string with the username and the password
-        let loginString = NSString(format: "%@:%@", username, password)
-        // Login information encodings
-        let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
-        let base64LoginString = loginData.base64EncodedStringWithOptions([])
-        // Setting the request header field: authorization header field is used.
-        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
-        
-        //// 3. Setting up data you want to send to the server
-        // If the user filled out blanks, then proceed with those infomations.
-        if let number_text = student_number.text, name_text = student_name.text, department_text = student_department.text, lecture_text = student_lecture.text {
-            // to == receiver's name<email>
-            // &subject == desired subject
-            // Subject&text == body
-            let bodyStr = "from=Mailgun Sandbox <postmaster@sandbox15cbe9dbdda946eebdea18594ab7d313.mailgun.org>&to=Hyebin Yoon <hbyoon28@gmail.com>&subject=Priming Subject&text=Student Number - \(number_text)\n Name - \(name_text)\n Department - \(department_text)\n Lecture - \(lecture_text)"
-            // Appending the above data to the request body (do encoding here too.)
-            request.HTTPBody = bodyStr.dataUsingEncoding(NSUTF8StringEncoding);
-        }
-        
-        //// 4. Create task object to implement the request.
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
-            // When the server responses with an error, then print that error.
-            if let error = error {
-                print(error)
-            }
-            // When the server responses positively, print those too.
-            if let response = response {
-                print("url = \(response.URL!)")
-                print("response = \(response)")
-                let httpResponse = response as! NSHTTPURLResponse
-                print("response code = \(httpResponse.statusCode)")
-            }
-        })
-        
-        //// 5. Finally, execute the task object.
-        task.resume()
-    }
+    /////////////////////////
+    // To sub_view_controller
+    /////////////////////////
     
-    
-    
-    //////////////////////////////
-    // Left-handed button callback
-    //////////////////////////////
-    @IBAction func left_button(sender: AnyObject) {
+    //// From main_view_controller to sub_view_controller
+    override func prepareForSegue(segue:UIStoryboardSegue, sender: AnyObject?) {
+        // Setting destination as sub_view_controller
+        let destination = segue.destinationViewController as! sub_view_controller
+        // Stashing user input data to the dictionary
+        destination.passing_data["student_number"] = student_number.text
+        destination.passing_data["student_name"] = student_name.text
+        destination.passing_data["student_department"] = student_department.text
+        destination.passing_data["student_lecture"] = student_lecture.text
+        destination.passing_data["left_button"] = String(left_handed.enabled)
     }
     
     
--- Priming/sub_view_controller.swift
+++ Priming/sub_view_controller.swift
@@ -15,28 +15,122 @@ import UIKit
 class sub_view_controller: UIViewController {
     
     
-
-
+    
+    
     
     ///////////////////////// Outlets /////////////////////////
     @IBOutlet weak var label_text: UILabel!
+    @IBOutlet weak var x_button: UIButton!
+    @IBOutlet weak var o_button: UIButton!
+    @IBOutlet weak var start_button: UIButton!
+    
+    //// The dictionary came from main_view_controller
+    var passing_data = [String: String]()
+    // Variables will capture informations from the dictionary
+    var student_number:String?
+    var student_name:String?
+    var student_department:String?
+    var student_lecture:String?
+    var left_button:String?
     
     
     
-
+    
     
     ///////////////////////// Actions /////////////////////////
+    
+    
+    
+    ///////////////
+    // Start button
+    ///////////////
+    @IBAction func start_button(sender: AnyObject) {
+        
+        //// 1. Send a request to server (Mailgun).
+        let request: NSMutableURLRequest = NSMutableURLRequest(URL: NSURL(string: "https://api.mailgun.net/v3/sandbox15cbe9dbdda946eebdea18594ab7d313.mailgun.org/messages")!)
+        
+        // Set a type of communication between a client (user) and the server.
+        request.HTTPMethod = "POST" // 'POST' request is used to send data to the server.
+        
+        //// 2. You have to authenticate (ID, Password) to the Mailgun to send mails through it.
+        let username = "api"
+        let password = "key-13bf8dc7873358d4352e591919b48616"
+        // Making a login information string with the username and the password
+        let loginString = NSString(format: "%@:%@", username, password)
+        // Login information encodings
+        let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
+        let base64LoginString = loginData.base64EncodedStringWithOptions([])
+        // Setting the request header field: authorization header field is used.
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        
+        //// 3. Setting up data you want to send to the server
+        // If the user filled out blanks, then proceed with those infomations.
+        if let number_text = self.student_number, name_text = self.student_name, department_text = self.student_department, lecture_text = self.student_lecture {
+            // to == receiver's name<email>
+            // &subject == desired subject
+            // &text == body
+            let bodyStr = "from=Mailgun Sandbox <postmaster@sandbox15cbe9dbdda946eebdea18594ab7d313.mailgun.org>&to=EMCS <koreamatlab@gmail.com>&subject=Priming: \(name_text)&text=Name: \(name_text)\nStudent Number: \(number_text)\n Department: \(department_text)\n Lecture: \(lecture_text)"
+            // Appending the above data to the request body (do encoding here too.)
+            request.HTTPBody = bodyStr.dataUsingEncoding(NSUTF8StringEncoding);
+        }
+        
+        //// 4. Create task object to implement the request.
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
+            // When the server responses with an error, then print that error.
+            if let error = error {
+                print(error)
+            }
+            // When the server responses positively, print those too.
+            if let response = response {
+                print("url = \(response.URL!)")
+                print("response = \(response)")
+                let httpResponse = response as! NSHTTPURLResponse
+                print("response code = \(httpResponse.statusCode)")
+            }
+        })
+        
+        //// 5. Finally, execute the task object.
+        task.resume()
+    }
+    
+    
+    
+    ///////////
+    // X button
+    ///////////
     @IBAction func x_button(sender: AnyObject) {
     }
+    
+    
+    
+    ///////////
+    // O button
+    ///////////
     @IBAction func o_button(sender: AnyObject) {
     }
     
-    //// Do any additional setup after loading the view.
+    
+    
+    //////////////////////////////////////////////////
+    // Do any additional setup after loading the view.
+    //////////////////////////////////////////////////
     override func viewDidLoad() {
+        
+        //// From main_view_controller
         super.viewDidLoad()
+        // Distributing informations (heuristic indexing)
+        self.student_number = [String](passing_data.values)[1]
+        self.student_name = [String](passing_data.values)[4]
+        self.student_department = [String](passing_data.values)[3]
+        self.student_lecture = [String](passing_data.values)[2]
+        self.left_button = [String](passing_data.values)[0]
     }
     
-    //// Dispose of any resources that can be recreated.
+    
+    
+    //////////////////////////////////////////////////
+    // Dispose of any resources that can be recreated.
+    //////////////////////////////////////////////////
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
